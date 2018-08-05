2a14bcbcb94e8b89485ecce53565f448dd26dd52
Alan Bell <alan.bell@optalysys.com>
2018-08-05 19:20:50 +0100

    Iterates through DC Block sizes and snippet sizes

--- Make_Snippets.py
+++ Make_Snippets.py
@@ -4,9 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 from scipy import misc, ndimage
-from math import cos, radians
-from PIL import Image
-import subprocess
+
 
 
 # Makes snippets (Small NxM random patterns) tiled across the Input SLM and the corresponding Binary Filter
@@ -32,8 +30,9 @@ def MakeReferencePage(Ref, snippet, Spacing):
             Ref[y : y+snippet.shape[0], x : x+snippet.shape[1]] = snippet
 
     plt.figure(1)
-    plt.cla()
+
     ax1 = plt.gca()
+    ax1.cla()
     ax1.xaxis.set_visible(False)
     ax1.yaxis.set_visible(False)
     ax1.set_xticks([])
@@ -42,7 +41,8 @@ def MakeReferencePage(Ref, snippet, Spacing):
     plt.imshow(Ref, cmap='gist_gray', vmin=0, vmax=1)
     plt.draw()
     plt.pause(1)
-    plt.imsave('InputSLM.png', Ref, cmap=plt.cm.gray, vmin=0, vmax = 1)
+    plt.imsave('Snippet_'+str(snippet.shape[1])+'x'+str(snippet.shape[0])+'.png', Ref, cmap=plt.cm.gray, vmin=0, vmax = 1)
+    plt.clf()
 
     return Ref
 
@@ -60,43 +60,53 @@ def ComputeFilter(target, filtersize):
     print('targetX %2.1f : targetY %2.1f  : PadX  %2.1f : PadY %2.1f ' % (targetX, targetY, PadX, PadY))
 
     paddedtarget = np.pad(target, [(PadY, ), (PadX, ) ], mode='constant', constant_values = 1.0)
-    plt.figure(3)
-    ax3 = plt.gca()
-    ax3.xaxis.set_visible(False)
-    ax3.yaxis.set_visible(False)
-    ax3.set_xticks([])
-    ax3.set_yticks([])
-    plt.imsave('PaddedTarget.png', paddedtarget, cmap=plt.cm.gray, vmin=0., vmax=1.0)
-
     targetFT = np.fft.fftshift(np.fft.fft2(np.fft.ifftshift(paddedtarget)))
     conjugatePhaseTargetFT = -(np.angle(targetFT))
     binaryConjugateTargetFT = ((conjugatePhaseTargetFT > 0.0)).astype(int)
 
-    plt.imsave('FourierFilter.png', binaryConjugateTargetFT, cmap=plt.cm.gray, vmin=0., vmax=1.0)
 
     return binaryConjugateTargetFT
 
 #oooooooooooooooOOOOOOOOOOOOOOOOOOOOOOOOOOoooooooooooooooooooooooooo
 
 
+def ApplyDCBlock(filter, dc):
 
+    Center = [np.floor(0.5 * filter.shape[0]), np.floor(0.5 * filter.shape[1])]
+    # Convert Center to integers
+    Center = [int(c) for c in Center]
+    filter[Center[0]-dc:Center[0]+dc, Center[1]-dc:Center[1]+dc] = 1
+    return filter
 
+#oooooooooooooooOOOOOOOOOOOOOOOOOOOOOOOOOOoooooooooooooooooooooooooo
+
+def InitializeReference(RefSize):
+    Ref = np.zeros(RefSize)
+    return Ref
 # Make some variables
 
 # Shape of the snippets (Doesn't have to be square!)
-DataShape = [32,32]
+DataShape = ([16,16], [32,32], [16, 64], [16, 128], [100, 100])
 # Shape of the input image (size of the SLM)
-Ref = np.zeros((1080, 1920))
-
+RefSize = [1100 , 2000]
 # spacing between snippet images when tiled on the input image
 Spacing = 100
 
 # Size of the filter image (FourierFilter.png)
 filtersize = 1000
 
+DCBlock = [5,10,20,50]
 
 
 
-rawData = GenerateRandomData(DataShape)
-Ref = MakeReferencePage(Ref, rawData, Spacing)
-ComputeFilter(rawData, filtersize)
+
+for ds in DataShape:
+    Ref = InitializeReference(RefSize)
+    rawData = GenerateRandomData(ds)
+    Ref = MakeReferencePage(Ref, rawData, Spacing)
+    filter = ComputeFilter(rawData, filtersize)
+    plt.imsave('Snippet_'+str(ds[1])+'x'+str(ds[0])+'_No_DCBlock.png', filter, cmap=plt.cm.gray, vmin=0., vmax=1.0)
+
+    for dc in DCBlock:
+        filter = ApplyDCBlock(filter, dc)
+        plt.imsave('Snippet_'+str(ds[1])+'x'+str(ds[0])+'_DCBlock_'+str(dc)+'x'+str(dc)+'.png', filter, cmap=plt.cm.gray, vmin=0., vmax=1.0)
